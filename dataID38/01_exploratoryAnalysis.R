170532104fd3abeb823eec3dea748cafea0e9dc6
Umar Niazi <uhkniazi@gmail.com>
2019-01-28 13:29:44 +0000

    EDA for the data matrix done, use both treatment and patient ids as covariates in the model

--- dataID38/01_exploratoryAnalysis.R
+++ dataID38/01_exploratoryAnalysis.R
@@ -10,36 +10,28 @@ db = dbConnect(MySQL(), user='rstudio', password='12345', dbname='Projects', hos
 dbListTables(db)
 
 # select the right table using data and project id
-g_did = 25
+g_did = 38
 dbListFields(db, 'MetaFile')
 
-dfFiles = dbGetQuery(db, 'select * from MetaFile where idData=25')
+dfFiles = dbGetQuery(db, 'select * from MetaFile where idData=38')
 
 dfFiles
 
 ## load the 2 files
 n = paste0(dfFiles$location, dfFiles$name)
 dfCovariates = read.csv(n[1], header=T)
-dfData = read.csv(n[2], header=T, row.names=1, sep = '\t')
+dfData = read.csv(n[2], header=T, row.names=1, sep = ',')
 
 dim(dfCovariates)
 dim(dfData)
 
 colnames(dfData)
 str(dfCovariates)
-table(as.character(dfCovariates$SampleName) %in% colnames(dfData))
+table(as.character(dfCovariates$SampleID) %in% colnames(dfData))
 
-## drop the outlier sample according to first pass analysis
-## IL2217_2
-# dfCovariates = dfCovariates[!as.character(dfCovariates$SampleName) == 'IL2217_2',]
-# dfData = dfData[,!colnames(dfData) %in% 'IL2217_2']
-# i = match(as.character(dfCovariates$SampleName), colnames(dfData))
-
-## there is a discrepency where we have some samples duplicated in the covariates table
-## this is due to the those samples having multiple lanes but being merged at the count table
-## after doing first pass analysis, drop these duplicated covariates
-i = match(colnames(dfData), as.character(dfCovariates$SampleName))
+i = match(colnames(dfData), as.character(dfCovariates$SampleID))
 dfCovariates = dfCovariates[i,]
+identical(as.character(dfCovariates$SampleID), colnames(dfData))
 
 ## first normalise the data
 mData = as.matrix(dfData)
@@ -55,14 +47,8 @@ library(DESeq2)
 sf = estimateSizeFactorsForMatrix(mData)
 mData.norm = sweep(mData, 2, sf, '/')
 
-# ## duplicate the missing samples to make even with covariates
-# mData = mData[,i]
-# dim(mData)
-# mData.norm = mData.norm[,i]
-
-#colnames(mData) = gsub(pattern = '\\.\\d', '', colnames(mData))
-identical(colnames(mData), as.character(dfCovariates$SampleName))
-identical(colnames(mData.norm), as.character(dfCovariates$SampleName))
+identical(colnames(mData), as.character(dfCovariates$SampleID))
+identical(colnames(mData.norm), as.character(dfCovariates$SampleID))
 
 ## load CDiagnostics and test
 ## compare the normalised and raw data
@@ -80,10 +66,9 @@ oDiag.2 = CDiagnosticPlots(log(mData+1), 'Original')
 # the batch variable we wish to colour by, 
 # this can be any grouping/clustering in the data capture process
 # e.g. in this case it is different lanes/machines
-fBatch = factor(dfCovariates$PatientID)
-fBatch = factor(dfCovariates$Treatment)
-fBatch = factor(dfCovariates$Lane)
-fBatch = factor(dfCovariates$RunID)
+str(dfCovariates)
+fBatch = factor(dfCovariates$Donor)
+fBatch = factor(dfCovariates$Group)
 
 ## compare the 2 methods using various plots
 par(mfrow=c(1,2))
@@ -117,22 +102,103 @@ plot.PCA(oDiag.2, fBatch)
 plot.dendogram(oDiag.1, fBatch, labels_cex = 0.7)
 plot.dendogram(oDiag.2, fBatch, labels_cex = 0.7)
 
-## extreme values
-oDiag.1 = calculateExtremeValues(oDiag.1)
-oDiag.2 = calculateExtremeValues(oDiag.2)
-m1 = mGetExtremeValues(oDiag.1)
-m2 = mGetExtremeValues(oDiag.2)
+# ## extreme values
+# oDiag.1 = calculateExtremeValues(oDiag.1)
+# oDiag.2 = calculateExtremeValues(oDiag.2)
+# m1 = mGetExtremeValues(oDiag.1)
+# m2 = mGetExtremeValues(oDiag.2)
 
-## samples with most extreme values
-apply(m1, 2, function(x) sum(x > 0))
-apply(m2, 2, function(x) sum(x > 0))
+# ## samples with most extreme values
+# apply(m1, 2, function(x) sum(x > 0))
+# apply(m2, 2, function(x) sum(x > 0))
+# 
+# ## variables that are contributing to this
+# v1 = apply(m1, 1, function(x) sum(x > 0))
+# v2 = apply(m2, 1, function(x) sum(x > 0))
+# 
+# which(v1 > 0)
+# which(v2 > 0)
 
-## variables that are contributing to this
-v1 = apply(m1, 1, function(x) sum(x > 0))
-v2 = apply(m2, 1, function(x) sum(x > 0))
 
-which(v1 > 0)
-which(v2 > 0)
+plot(oDiag.1@lData$PCA$sdev)
+mPC = oDiag.1@lData$PCA$x[,1:4]
+
+## try a linear mixed effect model to account for varince
+library(lme4)
+dfData = data.frame(mPC)
+dfData = stack(dfData)
+dfData$fBatch = dfCovariates$Group
+dfData$fAdjust1 = dfCovariates$Donor
+
+dfData$Coef = factor(dfData$fBatch:dfData$ind)
+dfData$Coef.adj1 = factor(dfData$fAdjust1:dfData$ind)
+
+str(dfData)
+plot(density(dfData$values))
+
+fit.lme1 = lmer(values ~ 1  + (1 | Coef) + (1 | Coef.adj1), data=dfData)
+summary(fit.lme1)
+
+fit.lme2 = lmer(values ~ 1  + (1 | Coef), data=dfData)
+summary(fit.lme2)
+
+anova(fit.lme1, fit.lme2)
+
+plot((fitted(fit.lme1)), resid(fit.lme1), pch=20, cex=0.7)
+lines(lowess((fitted(fit.lme1)), resid(fit.lme1)), col=2)
+hist(dfData$values, prob=T)
+lines(density(fitted(fit.lme1)))
+
+plot((fitted(fit.lme2)), resid(fit.lme2), pch=20, cex=0.7)
+lines(lowess((fitted(fit.lme2)), resid(fit.lme2)), col=2)
+hist(dfData$values, prob=T)
+lines(density(fitted(fit.lme2)))
+lines(density(fitted(fit.lme1)), col=2)
 
 
+## fit model with stan
+library(rstan)
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+
+stanDso = rstan::stan_model(file='tResponse2RandomEffectsNoFixed.stan')
+
+### try a t model without mixture
+lStanData = list(Ntotal=nrow(dfData), Nclusters1=nlevels(dfData$Coef),
+                 Nclusters2=nlevels(dfData$Coef.adj1),
+                 NgroupMap1=as.numeric(dfData$Coef),
+                 NgroupMap2=as.numeric(dfData$Coef.adj1),
+                 y=dfData$values, 
+                 gammaShape=0.5, gammaRate=1e-4)
+
+fit.stan = sampling(stanDso, data=lStanData, iter=10000, chains=4,
+                    pars=c('betas', 'sigmaRan1', 'sigmaRan2', 
+                           'nu', 'sigmaPop', 'mu',
+                           'rGroupsJitter1', 'rGroupsJitter2'),
+                    cores=4, control=list(adapt_delta=0.99, max_treedepth = 12))
+
+print(fit.stan, c('betas', 'sigmaRan1', 'sigmaRan2', 'sigmaPop', 'nu'), digits=3)
+traceplot(fit.stan, 'betas')
+traceplot(fit.stan, 'sigmaRan1')
+traceplot(fit.stan, 'sigmaRan2')
+m = cbind(extract(fit.stan)$sigmaRan1, extract(fit.stan)$sigmaRan2) 
+dim(m)
+m = log(m)
+colnames(m) = c('Treatment', 'Patient')
+pairs(m, pch=20, cex=0.5, col='grey')
+library(lattice)
+df = stack(data.frame(m))
+histogram(~ values | ind, data=df, xlab='Log SD')
+
+
+hist(dfData$values, prob=T)
+plot(density(dfData$values))
+mFitted = extract(fit.stan)$mu
+
+apply(mFitted[sample(1:nrow(mFitted), size = 100), ], 1, function(x) lines(density(x)))
+
+m = colMeans(mFitted)
+r = dfData$values - m
+plot(m, r, pch=20)
+lines(lowess(m, r), col=2)
 
